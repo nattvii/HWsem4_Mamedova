fa871ae384f24ceb7a8bd40e0949eeb9fe558a47
nattvii <natt.mmd@gmail.com>
2023-01-19 18:51:31 +0300

    ex02

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Fill in a number: ");
+int i = Convert.ToInt32(Console.ReadLine());
+int summ = 0;
+
+while (i > 0)
+    {
+        int n = i % 10;
+        i = i / 10;
+        summ = summ + n;
+    }
+Console.WriteLine("The answer is: " + summ); 
